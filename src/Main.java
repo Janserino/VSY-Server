c32be7628ad984e0b382961ab248363b6534fc0a
paraidomat <s-k-y.f-i-r-e@gmx.de>
2015-10-20 20:17:50 +0200

    2do erweitert

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
-/* 2do
-    foobar
- */
+/* 2 do
+    - Datenbankanbindung
+    - Datenbankstruktur
+    - Funktion zur Kalkulation von Tips
+        - Sollte bei durchschnittlich schlechter Bewertung und guten aktuellen Score mehr Tips zurückgeben
+    - Anbindung für das Handy
+    - HA Cluster (floating IP)
+    - Easter Egg implementieren
+    - Funktion zur Umkreissuche für die App
+        - App übermittelt GPS koordinaten
+        - Funktion sucht nach nächstem restaurant im Umkreis von x
+        - User entscheidet, ob er dort ist
+        - Wenn nein, neues Restaurant hinzufügen (braucht maske in der App)
+     - Speichern der Bewertung des aufenthalts
+        - Survey nach dem Besuch
+        - Eventuell mit Pushbenachrichtigung nach x Minuten, wenn keine Bewertung oder gar kein Input erfolg
+            - soll verfälschte Werte verhindern
+    - Abrufen von Bewertungen der Restaurants im Umkreis
+        - Balken in der app der grün nach rechts wächst, wenn restaurant gut
+        - Balken in der app, der rot nach links wächst, wenn restaurant schlecht
+*/
 
 public class Main {
 
